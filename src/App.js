1a6e2c91ddeed8101bcba87d524d3193494ea838
Your Name <abbas@lendsmart.ai>
2020-08-22 01:36:14 +0530

    Fix:second commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Buddy PIpeline.
+          Welcome to Lendsmart
         </p>
         <a
           className="App-link"
