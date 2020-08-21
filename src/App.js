43d3764851a637cf7fc43e71ffcbfd4a1c66c2f9
Your Name <abbas@lendsmart.ai>
2020-08-22 01:46:16 +0530

    Fix:sec

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Rehman&Co
+          Welcome to Palace
         </p>
         <a
           className="App-link"
