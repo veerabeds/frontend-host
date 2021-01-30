37a8eb2f6fd3683f4ee889ff34c8bce81fe2c289
veerabeds <48977086+veerabeds@users.noreply.github.com>
2021-01-30 23:57:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Lendsmart
+          Welcome to Devops course
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://dcedusolution.com"
           target="_blank"
           rel="noopener noreferrer"
         >
